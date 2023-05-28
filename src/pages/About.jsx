704fafc7b7b50cf941c866ef7d64184d036e58c4
El-Behery <dev.elbehery@gmail.com>
2023-05-28 11:54:20 +0300

    responsive changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 const About = () => {
-  return <div className="h-[50vh] bg-black bg-opacity-75 pt-20">About</div>;
+  return <div className="bg-black bg-opacity-75 pt-16">About</div>;
 };
 
 export default About;
--- src/pages/DecorativePieces.jsx
+++ src/pages/DecorativePieces.jsx
@@ -1,9 +1,5 @@
 const DecorativePieces = () => {
-  return (
-    <div className="h-[50vh] bg-black bg-opacity-75 pt-20">
-      DecorativePieces
-    </div>
-  );
+  return <div className="bg-black bg-opacity-75 pt-16">Decorative Pieces</div>;
 };
 
 export default DecorativePieces;
